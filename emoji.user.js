8f111232f29f329f2b4d895f173b006662b5084f
Nizrab <106761295+Nizrabed@users.noreply.github.com>
2023-09-10 01:03:32 +0300

    Better emoji loading system

--- emoji.user.js
+++ emoji.user.js
@@ -23,23 +23,30 @@ window.addEventListener('load', function () {
     .then(response => response.json())  // log response to jason
     .then(data => {
         console.log("fetched data:", data);  // log fetched data
+
+        // complete the emoji urls
+        data = JSON.parse(JSON.stringify(data).replace(': "', 'https://raw.githubusercontent.com/juralumin/swagmoji/main/assets/'))
+
+        console.log("Completed emoji urls:", data)  // log new data
+
         Object.assign(emojiDB, data);
 
-        // log each emoji's name for 2% easier debugging ahhahhahahahhahahaha
-        console.log("loaded emojis:");
-        Object.keys(emojiDB).forEach(key => {
-            console.log(key);
-        });
+        // // log each emoji's name for 2% easier debugging ahhahhahahahhahahaha
+        // console.log("Loaded emojis:");
+        // Object.keys(emojiDB).forEach(key => {
+        //     console.log(key);
+        // });
 
-        // use updated emoji db
+        // use updated emojiDB
         App.chat.markdownProcessor.use(emojiPlugin, {
             emojiDB
         });
     })
     .catch(error => {
-        console.error("error fetching emoji data!", error);  // log any undesired happenings
+        App.alert("Error fetching custom emoji data. Try reloading Pxls. -swagemoji")
+        console.error("Error fetching emoji data!", error);  // log any undesired happenings
     });
 });
 
 // if emojis are all funky, clear your cache.
-// also DON'T add emojis with UPPERCASE characters. they should all be lowercase
+// also DON'T add emojis with UPPERCASE characters. they should all be lowercase (weird pxls thing)
