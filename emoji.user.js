a7b35b6b012401e91c638640b064cd145e2eb07c
Nizrab <106761295+Nizrabed@users.noreply.github.com>
2023-09-09 19:25:37 +0300

    big change

--- emoji.user.js
+++ emoji.user.js
@@ -1,24 +1,42 @@
 // ==UserScript==
 // @name         swagmoji
-// @version      1.0
+// @version      1.1
 // @description  client-side emojis to ever so slightly better your pxls.space experience
-// @author       Nizrab
+// @author       Nizrab and Jen
 // @match        https://pxls.space/
 // @icon         https://pxls.space/favicon.ico
 // ==/UserScript==
+
 const emojiPlugin = pxlsMarkdown.plugins.emoji
 
-window.addEventListener('load', function ()
-{
-    fetch('https://raw.githubusercontent.com/juralumin/swagmoji/main/emojis.json', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-    },
-})
-   .then(response => Object.assign(emojiDB, response.json()));
+// WHERE IS PREMP
 
-    App.chat.markdownProcessor.use(emojiPlugin, {
-        emojiDB
+window.addEventListener('load', function () {
+    console.log("loading...");  // log when stuff's loaded
+
+    fetch('https://raw.githubusercontent.com/juralumin/swagmoji/main/emojis.json', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then(response => response.json())  // log response to jason
+    .then(data => {
+        console.log("fetched data:", data);  // log fetched data
+        Object.assign(emojiDB, data);
+
+        // log each emoji's name for 2% easier debugging ahhahhahahahhahahaha
+        console.log("Loaded emojis:");
+        Object.keys(emojiDB).forEach(key => {
+            console.log(key);
+        });
+
+        // use updated emoji db
+        App.chat.markdownProcessor.use(emojiPlugin, {
+            emojiDB
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching emoji data: ", error);  // log any undesired happenings
     });
 });
